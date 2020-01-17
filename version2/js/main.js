f6a476e81179751270d7db408bfb5dd68afc6c8f
Noland Seigler <nolandseigler@gmail.com>
2020-01-17 01:19:35 -0600

    it is doing the swapping but it overwrites what it is dropped on

--- version2/js/main.js
+++ version2/js/main.js
@@ -18,7 +18,7 @@
         const makeTiles = (number) => {
             let i = 1;
             while (i < number + 1) {
-                $('#wrapper').append(`<div class="tile"><div class="'number">${i}</div></div>`);
+                $('#wrapper').append(`<div class="tile-container" id='tile-container-${i}'><div class="tile" id='tile-${i}' draggable="true">${i}</div></div>`);
                 i++
             };
         };
@@ -29,6 +29,66 @@
                 $(this).css('background-color', hex());
             }
         });
+
+        const tile = $('.tile');
+        const tileContainer = $('.tile-container');
+
+        //drag functions
+        let dragTileId;
+        let dragTileContainerId;
+        const dragStart = function () {
+            $(this).addClass('hold');
+            dragTileId = $(this).attr('id');
+            dragTileContainerId = $(this).parent().attr('id');
+            console.log(dragTileContainerId);
+            // let realThis = $(this);
+            // setTimeout(function () {
+            //     realThis.addClass('invisible');
+            // }, 0);
+        };
+
+        const dragEnd = function () {
+            console.log('dragend');
+            // let realThis = $(this);
+            // setTimeout(function () {
+            //     realThis.removeClass('invisible');
+            // }, 0);
+        };
+
+        const dragOver = function (e) {
+            e.preventDefault();
+        };
+
+        const dragEnter = function (e) {
+            e.preventDefault()
+            $(this).addClass('hover');
+        };
+
+        const dragLeave = function () {
+            $(this).removeClass('hover');
+        };
+
+        const dragDrop = function () {
+            console.log($(this).attr('id'));
+            let swap = $(this).children().attr('id');
+            console.log(swap);
+            $(dragTileContainerId).html($(`#${swap}`));
+            $(this).html($(`#${dragTileId}`));
+        };
+
+        //containers listeners
+        tileContainer.on('dragover', dragOver);
+        tileContainer.on('dragenter', dragEnter);
+        tileContainer.on('dragleave', dragLeave);
+        tileContainer.on('drop', dragDrop);
+
+        //tile listeners
+
+        tile.on('dragstart', dragStart);
+        tile.on('dragend', dragEnd);
+
+
+
 
 
     });

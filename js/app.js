bd532f35cb850c68b4396ff1d759b286e6e1729b
Noland Seigler <nolandseigler@gmail.com>
2020-01-16 21:19:53 -0600

    initial files setup. following tutorial on first vid

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// import './css/style.css';
+import PicturePuzzle from "./PicturePuzzle";
+
+const picturePuzzle = new PicturePuzzle(
+    document.querySelector('#'),
+    'img/fajita.jpeg',
+    600
+);
+
+picturePuzzle.setup();

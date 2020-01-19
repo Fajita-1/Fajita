6ac2762d837fda0dbdccf03ec281769ead2a0ab7
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-19 15:34:31 -0600

    added version3

---
+++ version3/src/js/app.js
@@ -0,0 +1,35 @@
+import '../scss/style.scss';
+import PicturePuzzle from './PicturePuzzle'
+
+const picturePuzzle = new PicturePuzzle(
+    document.querySelectorAll('#puzzle-wrapper > div')[0],
+    'http://52.24.98.51/wp-content/uploads/2017/01/drone.jpg',
+    600
+);
+const picturePuzzle2 = new PicturePuzzle(
+    document.querySelectorAll('#puzzle-wrapper > div')[1],
+    'https://www.tesla.com/sites/default/files/images/homepage/20180710/ms/homepage-models.jpg?20181117',
+    600,
+    4
+);
+
+picturePuzzle.onSwap = function(movements){
+    console.log(movements);
+};
+
+const modal = document.querySelector('#success-modal');
+modal.style.display = 'block';
+
+picturePuzzle.onFinished = function() {
+    console.log("Show good job dialog");
+
+    setTimeout(() => {
+        modal.classList.add('open');
+        this.el.classList.add('blur-it')
+    }, 500);
+    modal.querySelector('.trigger').onclick = () => {
+        modal.classList.remove('open');
+        this.el.classList.remove('blur-it');
+    }
+
+};
---
+++ version3/webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        PicturePuzzle: './src/js/PicturePuzzle.js'
+    },
+    output: {
+        path: __dirname + '/dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+
+};

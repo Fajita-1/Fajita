e7cb8c5f4f97508abe156cadef0f0f4cda818ab0
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-19 15:57:17 -0600

    updated

--- version1/webpack.config.js
+++ version1/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/PicturePuzzle.js',
     output: {
-        filename: 'main.js',
+        filename: 'cell.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };

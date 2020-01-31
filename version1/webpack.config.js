116c1023fae10d1392a3a77033b2a685f9087bb8
Noland Seigler <nolandseigler@gmail.com>
2020-01-30 21:21:27 -0600

    deleted my version one. version 3 is correct and works

--- version1/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './src/PicturePuzzle.js',
-    output: {
-        filename: 'cell.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-};
